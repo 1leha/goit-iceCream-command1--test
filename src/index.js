484ea1ad56791f77aa14fc08e5ccb0e06410748f
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-08-07 00:05:21 +0400

    adding masonry layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-import './js/customerReviewsSlider';
+// import './js/customerReviewsSlider';
+import './js/masonryLayoutGallery';
--- src/js/customerReviewsSlider.js
+++ src/js/customerReviewsSlider.js
@@ -1,10 +1,3 @@
-// Тема по умолчанию
-// import '@splidejs/splide/src/css/themes/default/index.scss';
-
-// or other themes
-// import '@splidejs/splide/css/skyblue';
-// import '@splidejs/splide/css/sea-green';
-
 // or only core styles
 import '@splidejs/splide/src/css/core/index.scss';
 
@@ -18,8 +11,8 @@ const splideOptions = {
   rewindByDrag: true,
   arrows: false,
   easing: 'cubic-bezier(0.25, 1, 0.5, 1)',
-  //   autoplay: true,
-  //   interval: 5000,
+  autoplay: true,
+  interval: 5000,
   classes: {
     pagination: 'splide__pagination pagination',
     page: 'splide__pagination__page pagination__page',
